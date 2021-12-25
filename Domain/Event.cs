1a58fca37df7d50b4910959269e8ff2fce89f050
permalik <permalik@protonmail.com>
2021-12-24 19:19:04 -0600

    Implement DataContext

--- Domain/Event.cs
+++ Domain/Event.cs
@@ -7,7 +7,7 @@ namespace Domain
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Category { get; set; }
-    public string description { get; set; }
+    public string Description { get; set; }
     public string Venue { get; set; }
     public string City { get; set; }
     public string State { get; set; }
