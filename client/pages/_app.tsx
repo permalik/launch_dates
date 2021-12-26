c59d52b0d5a543e2770ac06765e83b07f6d2dc2c
permalik <permalik@protonmail.com>
2021-12-25 19:46:26 -0600

    Create event state and call events from database

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import axios from 'axios';
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/events').then(response => {
+      console.log(response);
+      setEvents(response.data);
+    })
+  }, []);
+
+  return <Component {...pageProps} events={events} />
 }
-
-export default MyApp
