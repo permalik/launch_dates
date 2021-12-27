43f845330fe0c63e205fe10335b47d170b3a4bb8
permalik <permalik@protonmail.com>
2021-12-27 06:40:00 -0600

    Update BaseApiController with IMediator protected class and pass to EventsController

--- API/Controllers/EventsController.cs
+++ API/Controllers/EventsController.cs
@@ -1,31 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Events;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
   public class EventsController : BaseApiController
   {
-    private readonly DataContext _context;
-    public EventsController(DataContext context)
-    {
-      _context = context;
-    }
-
     [HttpGet]
     public async Task<ActionResult<List<Event>>> GetEvents()
     {
-      return await _context.Events.ToListAsync();
+      return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Event>> GetEvent(Guid id)
     {
-      return await _context.Events.FindAsync(id);
+      return Ok();
     }
   }
 }
