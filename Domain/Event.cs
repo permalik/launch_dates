ce44e8f699752da856cc99eb8ba9a9b52c6f0e03
permalik <permalik@protonmail.com>
2021-12-30 00:26:12 -0600

    Create Event class in Domain project

--- Domain/Event.cs
+++ Domain/Event.cs
@@ -1,7 +1,16 @@
-﻿namespace Domain
+﻿using System;
+
+namespace Domain
 {
     public class Event
     {
-        
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Venue { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public DateTime Date { get; set; }
     }
 }
