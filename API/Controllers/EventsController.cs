324f60a50e61f370721aca659ae00069702cb6d1
permalik <permalik@protonmail.com>
2021-12-31 15:10:18 -0600

    Implement MediatR pattern and move logic to Application project

--- API/Controllers/EventsController.cs
+++ API/Controllers/EventsController.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Events;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class EventsController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IMediator _mediator;
 
-        public EventsController(DataContext context)
+        public EventsController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Event>>> GetEvents()
         {
-            return await _context.Events.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Event>> GetEvent(Guid id)
         {
-            return await _context.Events.FindAsync(id);
+            return Ok();
         }
     }
 }
